dcbdc528f3f94168454b82a331bfed25de929726
willforlin <willforlin@gmail.com>
2021-02-06 22:43:45 -0300

    agoravai

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name ;
+            Console.WriteLine("Qual é seu nome? ") ;
+
+            name = Console.ReadLine() ;
+            Console.WriteLine("seu nome é "  + name) ;
+
+
+        }
+    }
+}
